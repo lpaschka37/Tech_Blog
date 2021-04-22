c9c06f50ae6bb5267ad697db34079175aebab3ad
Luke Paschka <lpaschka37@gmail.com>
2021-04-21 22:40:01 -0500

    update to line 12 in connection file

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ if( process.env.NODE_ENV === "production" && process.env.JAWSDB_URL ){
   sequelize = new Sequelize(
     process.env.DB_NAME, 
     process.env.DB_USER, 
-    process.env.DB_PW, {
+    process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
       port: 3306
